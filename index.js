527196610105fe50ae939cffa067706d5a6b5b6b
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2023-01-13 14:42:17 +0300

    Created grid of squares

--- index.js
+++ index.js
@@ -8,9 +8,26 @@ appDiv.innerHTML = `<h1>Drawing with JS</h1>`;
 let canvas = document.querySelector('canvas');
 let context = canvas.getContext('2d');
 
+//This for loop draws 5 squares - width property adjusts square width 
+// The nexted loop creates an even grid of squares
+for(let i = 0; i < 5; i++){
+  for(let j = 0; j < 5; j++){
+    let width = 60;
+    let height = 60;
+    let gap = 20;
+    let x = 100 + (width + gap) * i;
+    let y = 100 + (height + gap) * j;
+
+    context.beginPath();
+    context.rect(x,y,width,height);
+    context.stroke();
+  }
+}
+
 //context.fillStyle = "orange";
 //context.fillRect(100, 100, 400, 400);
 
+/* //Code below creates a square and circle
 context.lineWidth = 4;
 context.beginPath();
 context.rect(100, 100, 400, 400);
@@ -19,3 +36,5 @@ context.stroke();
 context.beginPath();
 context.arc(300, 300, 100, 0, Math.PI * 2);
 context.stroke();
+*/
+
