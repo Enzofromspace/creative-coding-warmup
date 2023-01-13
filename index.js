0739d1d5cd2d398c37c411a1dc7cda1ef857ec5e
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2023-01-13 14:25:47 +0300

    Drew a rectangle and circle

--- index.js
+++ index.js
@@ -3,4 +3,19 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>Drawing with JS</h1>`;
+
+let canvas = document.querySelector('canvas');
+let context = canvas.getContext('2d');
+
+//context.fillStyle = "orange";
+//context.fillRect(100, 100, 400, 400);
+
+context.lineWidth = 4;
+context.beginPath();
+context.rect(100, 100, 400, 400);
+context.stroke();
+
+context.beginPath();
+context.arc(300, 300, 100, 0, Math.PI * 2);
+context.stroke();
